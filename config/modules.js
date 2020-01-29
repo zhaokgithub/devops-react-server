63124acd849332aac3ddbc62b58c4c17869309c1
zhaokai <820641374@qq.com>
2020-01-29 14:22:37 +0800

    update

--- config/modules.js
+++ config/modules.js
@@ -14,7 +14,6 @@ function getAdditionalModulePaths(options = {}) {
   const baseUrl = options.baseUrl;
 
   // We need to explicitly check for null and undefined (and not a falsy value) because
-  // TypeScript treats an empty string as `.`.
   if (baseUrl == null) {
     // If there's no baseUrl set we respect NODE_PATH
     // Note that NODE_PATH is deprecated and will be removed
